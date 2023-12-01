a22c992f9b48545b22616488fddd3bbdad8f4233
DergiSamayoa <dergio@gmail.com>
2023-12-01 10:35:54 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center p-2 font-bold">
+        Starter React - Vite - Javascript - TailwindCSS
+      </h1>
+    </>
+  );
+}
+
+export default App;
